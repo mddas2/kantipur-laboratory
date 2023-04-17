a0dd2b2697601949bf024359d1cdf0c06753fb42
mddas <mddas>
2023-04-17 12:50:33 +0545

    hide email

--- account/models.py
+++ account/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
     sex = models.CharField(max_length=255 , null=True) #male and female    
     phone = models.CharField(max_length=255 , null=True)
     email = models.EmailField(max_length=255, unique=True)
-    username = models.EmailField(max_length=255, unique=False)  
+    username = models.EmailField(max_length=255, unique=True)  
     apply_role_type = models.IntegerField(null=True) 
     image = models.ImageField(upload_to='user/profile', null=True)
 
@@ -25,7 +25,7 @@ class CustomUser(AbstractUser):
     )
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
 
-    USERNAME_FIELD = 'email'
+    # USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     def getRoleName(self):
