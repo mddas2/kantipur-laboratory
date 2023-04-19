eb35504bf306a064f100950bee84ed0200701548
Siddhartha45 <87509046+Siddhartha45@users.noreply.github.com>
2023-04-19 11:43:52 +0545

    added view to delete all permissions

--- management/urls.py
+++ management/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-from .views import LoginView
+from .views import LoginView,PermissionAllDelete
 
 
 router = DefaultRouter()
@@ -10,10 +10,13 @@ router.register('sample-form', views.SampleFormViewSet, basename="sample_form")
 router.register('commodity', views.CommodityViewSet, basename="commodity")
 router.register('commodity-category', views.CommodityCategoryViewSet, basename="commodity_category")
 router.register('users', views.CustomUserSerializerViewSet, basename="CustomUserSerializer")
+router.register('group', views.GroupViewSet, basename="group")
+router.register('permission', views.PermissionViewSet, basename="permission")
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('', views.Home,name='Home'),
-    path('auth/login/', LoginView.as_view())
+    path('auth/login/', LoginView.as_view()),
     # path('auth/',include('rest_framework.urls',namespace="login"),name='login_name'),
+    path('permission-all-delete/', PermissionAllDelete.as_view()),
 ]
--- management/views.py
+++ management/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .serializers import ClientCategorySerializer, SampleFormSerializer, CommoditySerializer, CommodityCategorySerializer,CustomUserSerializer,LoginSerializer
+from .serializers import ClientCategorySerializer, SampleFormSerializer, CommoditySerializer, CommodityCategorySerializer,CustomUserSerializer,LoginSerializer, GroupSerializer, PermissionSerializer
 from .models import ClientCategory, SampleForm, Commodity, CommodityCategory
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication,SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import Group, Permission
 
 from rest_framework import status
 from rest_framework.views import APIView
@@ -46,6 +47,28 @@ class CommodityCategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CommodityCategorySerializer
     authentication_classes = [SessionAuthentication]
     permission_classes = [IsAuthenticated]
+    
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    
+class PermissionViewSet(viewsets.ModelViewSet):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]    
+
+
+class PermissionAllDelete(APIView):
+    def get(self, request, format=None):
+        object = Permission.objects.all().delete()
+        serializer_class = PermissionSerializer
+        return Response({'detail': 'delete successful'}, status=status.HTTP_200_OK)
+
 
 def Home(request):
     return redirect('api/')
