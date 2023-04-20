bede4223c9da8e2a43f22520927d106a4e2f37a8
mddas <mddas>
2023-04-20 10:22:54 +0545

    JWT complete

--- mainproject/urls.py
+++ mainproject/urls.py
@@ -20,6 +20,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -40,10 +41,14 @@ urlpatterns = [
 
     path('api/account/', include('account.urls')),
     path('api/', include('management.urls')), 
-    
-    
+    path('gettoken/',TokenObtainPairView.as_view(),name="token_obtain_pair"),
+    path('refresh-token',TokenRefreshView.as_view(),name = 'refresg-token'),
+    path('token-verify/',TokenVerifyView.as_view(),name="token_verify"),
 
+    
     # path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+
 ]
