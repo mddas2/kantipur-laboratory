138668301740b16c46ce070412271ef5c2aef3d5
mddas <mddas>
2023-05-14 13:13:38 +0545

    report template

--- report/views.py
+++ report/views.py
@@ -1,10 +1,6 @@
 import os
 from django.conf import settings
 from django.http import HttpResponse
-from django.utils.translation import activate
-from reportlab.pdfgen import canvas
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
 from rest_framework import views
 
 from django.shortcuts import render
