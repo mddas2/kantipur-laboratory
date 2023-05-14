422e3858083edcf64821f53f67ec9a1a4174f8c0
mddas <mddas>
2023-05-15 02:25:07 +0545

    finish

--- report/urls.py
+++ report/urls.py
@@ -1,7 +1,9 @@
-from . import views
+from . import views,preeti_to_unicode
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [   
     path('get-report', views.ReportDownload.as_view()),
+    path('preeti-to-unicode', preeti_to_unicode.PreetiToUniCode.as_view()),
+    path('unicode-to-preeti', preeti_to_unicode.UnicodeToPreeti.as_view()),
 ]
