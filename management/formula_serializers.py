da5b145a994db64921a89b7be77a8fc18e2e2fd9
mddas <mddas>
2023-05-17 17:43:07 +0545

    formula json field

--- management/formula_serializers.py
+++ management/formula_serializers.py
@@ -10,7 +10,7 @@ class FormulaApiCalculateSerializer(serializers.Serializer):
     commodity_id = serializers.IntegerField()#serializers.CharField()
     parameter_id = serializers.IntegerField()
     sample_form_id = serializers.IntegerField()
-    formula_variable_fields_value = serializers.JSONField()
+    formula_variable_fields_value = serializers.CharField()
 
 class FormulaApiGetFieldSerializer(serializers.Serializer):
     commodity_id = serializers.IntegerField()
