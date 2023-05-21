9d5dda82ea461b037b61cabaaf2c4d7ef2f5358e
mddas <mddas>
2023-05-21 14:05:35 +0545

    notification modele

--- websocket/models.py
+++ websocket/models.py
@@ -2,19 +2,4 @@ from django.db import models
 from account.models import CustomUser
 
 # Create your models here.
-
-class Notification(models.Model):
-    notification_name = models.CharField(max_length=200,null=True)
-    from_notification = models.EmailField(max_length=200,null=True)
-    to_notification = models.ManyToManyField(CustomUser,related_name="notification",on_delete= models.CASCADE)
-    is_read = models.BooleanField(default=False)
-
-    
-    ROLE_CHOICES = (
-        ('SUPERADMIN', 'SUPERADMIN'),
-        ('SMU','SMU'),
-        ('SUPERVISOR', 'SUPERVISOR'),
-        ('ANALYST', 'ANALYST'),
-        ('USER', 'USER'),
-    )
-    group_notification = models.CharField(choices=ROLE_CHOICES, blank=True, null=True)
+c
