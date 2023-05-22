5717e7142a176839ef0dccff60dbb4eeb8ad2ec6
mddas <mddas>
2023-05-22 13:23:24 +0545

    model

--- account/models.py
+++ account/models.py
@@ -9,10 +9,14 @@ class CustomUser(AbstractUser):
     phone = models.CharField(max_length=15 , null=True)
     email = models.EmailField(max_length=255,unique=True)
     username = models.CharField(max_length=255)  
-    image = models.ImageField(upload_to='media/user/profile', null=True)
-    registration_document = models.ImageField(upload_to='media/user/registration', null=True)
-    is_verified = models.IntegerField(choices=[(0, 'Not verified'), (1, 'Verified')], default=0)
 
+    renew_document = models.ImageField(upload_to='media/user/renew_doument', null=True)
+    registration_document = models.ImageField(upload_to='media/user/registration', null=True)
+    department_name = models.CharField(max_length=255,null=True) 
+    department_address = models.CharField(max_length=255,null=True) 
+    registration_number = models.CharField(max_length=255,null=True) 
+
+    is_verified = models.IntegerField(choices=[(0, 'Not verified'), (1, 'Verified')], default=0)
     client_category = models.ForeignKey("management.ClientCategory",related_name="user",on_delete=models.CASCADE,null=True)    
 
     SUPERADMIN = 1
