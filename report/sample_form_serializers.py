363954a0eaa48a77c5c626103c4cab0bbb435de5
mddas <mddas>
2023-05-29 14:31:51 +0545

    after bug re again push if error re back to previous commit

--- report/sample_form_serializers.py
+++ report/sample_form_serializers.py
@@ -18,3 +18,31 @@ class SampleFormHasAnalystSerializer(serializers.ModelSerializer):
     class Meta:
         model = SampleForm
         fields = ['name','sample_has_parameter_analyst']
+
+
+from management.models import SampleForm, Commodity,SampleFormHasParameter
+from account.models import CustomUser
+from rest_framework import serializers
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email','id'] 
+
+class CommoditySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Commodity
+        fields = ['name']
+
+class SampleFormHasParameterReadSerializer(serializers.ModelSerializer):
+    analyst_user = CustomUserSerializer(read_only = True)
+    class Meta:
+        model = SampleFormHasParameter
+        fields = ['analyst_user'] 
+
+class SampleFormHasAnalystSerializer(serializers.ModelSerializer):
+    sample_has_parameter_analyst = SampleFormHasParameterReadSerializer(many=True,read_only=True)
+    commodity = CommoditySerializer(read_only = True)
+    class Meta:
+        model = SampleForm
+        fields = ['id','name','sample_has_parameter_analyst','commodity']
