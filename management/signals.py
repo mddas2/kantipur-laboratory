eab45101e0bc282ef5c3a23c0438964edfbf08c8
mddas <mddas>
2023-06-07 01:57:07 +0545

    sample form has parameter signaling

--- management/signals.py
+++ management/signals.py
@@ -42,14 +42,38 @@ def SampleFormParameterFormulaCalculatePreSave(sender, instance,created, **kwarg
 #         print("parameter is not null ")
 
 
-@receiver(pre_save, sender=SampleFormHasParameter)
-def SampleFormHasParameterPreSave(sender, instance, **kwargs):
-    if not instance.pk:
+@receiver(post_save, sender=SampleFormHasParameter)
+def SampleFormHasParameterPreSave(sender, instance,created **kwargs):
+    if created:
+        sample_form_obj = instance.sample_form    
+        # sample_form_parameter = sample_form_obj.parameters
+        sample_form_has_parameter = SampleFormHasParameter.objects.filter(sample_form_id = sample_form_obj.id,parameter = parameter_obj.id)
+        # check_parameter = sample_form_has_parameter.first().parameter.all()
+
+        # print(check_parameter)
+        status = "pendidng"
+        
+        parameters = sample_form_obj.parameters.all()    
+        for param in parameters:
+            sample_form_has_parameter_object = SampleFormHasParameter.objects.filter(sample_form_id = sample_form_obj.id,parameter = param.id)
+            if sample_form_has_parameter_object.exists():
+                status = "processing"
+            else:
+                status = "pending"
+                break
+
+        # from management.models import SampleForm
+
+
         instance.status = "processing"
-        sample_form_obj = instance.sample_form
-        sample_form_obj.status = "processing"
+        sample_form_obj.status = status
         sample_form_obj.form_available = "supervisor"
         sample_form_obj.save()
+
+
+
+
+        
 
 @receiver(pre_save, sender=SampleFormVerifier)
 def SampleFormHasVerifierPreSave(sender, instance, **kwargs):
