790d06609880b0811d8b11351638fbd5859636a5
mddas <mddas>
2023-06-13 00:49:34 +0545

    need id instead of sample form

--- management/verifier_serializer.py
+++ management/verifier_serializer.py
@@ -7,7 +7,7 @@ class SampleFormWriteVerifierSerilizer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate(self, data):
-        sample_form_id = data.get('sample_form')
+        sample_form_id = data.get('sample_form').id
         sample_form_obj = SampleForm.objects.get(id=sample_form_id)
         if sample_form_obj.is_analyst_test == False:
             raise serializers.ValidationError("Analyst have not sent sample test to supervisor.")
