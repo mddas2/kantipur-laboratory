d163c2db39d15212e348721f6d2da751179fbfd4
mddas <mddas>
2023-06-12 14:49:32 +0545

    most of completed

--- management/sample_form_has_parameter.py
+++ management/sample_form_has_parameter.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .serializers import SampleFormHasParameterReadSerializer,SampleFormHasParameterWriteSerializer
+from .serializers import SampleFormHasParameterReadSerializer,SampleFormHasParameterWriteSerializer,SampleFormReadAnalystSerializer
 from .models import SampleFormHasParameter
 from rest_framework import viewsets
 from rest_framework_simplejwt.authentication import JWTAuthentication
