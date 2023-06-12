e011689d779e951b6657b1b9608d787a7f46bc5c
mddas <mddas>
2023-06-12 12:59:08 +0545

    filtered analyst user

--- report/analyst_final_report_serializer.py
+++ report/analyst_final_report_serializer.py
@@ -36,8 +36,9 @@ class CompletedSampleFormHasAnalystSerializer(serializers.ModelSerializer):
         fields = ['id','name','supervisor_user','sample_has_parameter_analyst','commodity','status','created_date']
 
 class DetailSampleFormHasParameterRoleAsAnalystSerializer(serializers.ModelSerializer):
-    commodity = CommoditySerializer(read_only = True)
-    parameters = ParameterSerializer(read_only = True, many = True)
+    commodity = CommoditySerializer(read_only=True)
+    parameters = ParameterSerializer(read_only=True, many=True)
+
     class Meta:
         model = SampleForm
         fields = '__all__'
@@ -46,18 +47,18 @@ class DetailSampleFormHasParameterRoleAsAnalystSerializer(serializers.ModelSeria
         representation = super().to_representation(instance)
 
         sample_form_id = representation.get('id')
-
-        # Add extra response data for parameters field
         parameters_data = representation.get('parameters', [])
-      
+
+        request = self.context.get('request')
+        user = request.user if request else None
+
+        filtered_parameters_data = []
         for parameter_data in parameters_data:
             parameter_id = parameter_data.get('id')
-            # Check if the parameter exists in SampleFormHasParameter model
-            # print(parameter_id)
-            request = self.context.get('request')
-            user = request.user if request else None
 
-            sample_form_has_assigned_analyst_obj = SampleFormHasParameter.objects.filter(parameter=parameter_id, sample_form = sample_form_id,analyst_user=user)
+            sample_form_has_assigned_analyst_obj = SampleFormHasParameter.objects.filter(
+                parameter=parameter_id, sample_form=sample_form_id, analyst_user=user
+            )
             exists = sample_form_has_assigned_analyst_obj.exists()
             if exists:
                 analyst_obj = sample_form_has_assigned_analyst_obj.first().analyst_user
@@ -68,17 +69,19 @@ class DetailSampleFormHasParameterRoleAsAnalystSerializer(serializers.ModelSeria
                 parameter_data['first_name'] = first_name
                 parameter_data['last_name'] = last_name
                 parameter_data['assigned_date'] = created_date
-                
-                formula_obj_result = SampleFormParameterFormulaCalculate.objects.filter(sample_form_id=sample_form_id,parameter_id = parameter_id)
-                if formula_obj_result.count()>0:
+
+                formula_obj_result = SampleFormParameterFormulaCalculate.objects.filter(
+                    sample_form_id=sample_form_id, parameter_id=parameter_id
+                )
+                if formula_obj_result.count() > 0:
                     parameter_data['status'] = "completed"
                     parameter_data['result'] = formula_obj_result.first().result
                 else:
                     parameter_data['status'] = "processing"
                     parameter_data['result'] = '-'
 
+                parameter_data['exist'] = exists
+                filtered_parameters_data.append(parameter_data)
 
-            parameter_data['exist'] = exists
-
-        representation['parameters'] = parameters_data
+        representation['parameters'] = filtered_parameters_data
         return representation
