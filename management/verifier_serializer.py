9390b933486220ede13e2b4d7d69a9fbe4ac37bd
mddas <mddas>
2023-06-13 15:36:23 +0545

    finaal

--- management/verifier_serializer.py
+++ management/verifier_serializer.py
@@ -7,10 +7,11 @@ class SampleFormWriteVerifierSerilizer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate(self, data):
-        sample_form_id = data.get('sample_form').id
-        sample_form_obj = SampleForm.objects.get(id=sample_form_id)
-        if sample_form_obj.is_analyst_test == False:
-            raise serializers.ValidationError("Analyst have not sent sample test to supervisor.")
+        if data.get('sample_form') !=None :
+            sample_form_id = data.get('sample_form').id
+            sample_form_obj = SampleForm.objects.get(id=sample_form_id)
+            if sample_form_obj.is_analyst_test == False:
+                raise serializers.ValidationError("Analyst have not sent sample test to supervisor.")
 
         return data
     
