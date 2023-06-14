165f3f9d29e5b849812eda2867878dcdd54d219e
mddas <mddas>
2023-06-14 16:48:07 +0545

    report track

--- report/track.py
+++ report/track.py
@@ -8,20 +8,36 @@ from management import roles
 from rest_framework.exceptions import PermissionDenied
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter,OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class TrackSampleFormAPIView(views.APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    filter_backends = [SearchFilter,DjangoFilterBackend,OrderingFilter]
+    search_fields = ['name','owner_user','status','form_available','commodity__name']
+    ordering_fields = ['name','id']
+    filterset_fields = {
+        'name': ['exact', 'icontains'],
+        'owner_user': ['exact'],
+        'status': ['exact'],
+        'form_available': ['exact'],
+        'commodity_id': ['exact'],
+        'supervisor_user': ['exact'],
+        'created_date': ['date__gte', 'date__lte']  # Date filtering
+    }
+
     def get_queryset(self):
         user = self.request.user
   
         if user.role == roles.SMU:
-            return SampleForm.objects.all()
+            query = SampleForm.objects.all()
         elif user.role == roles.SUPERADMIN:
-            return SampleForm.objects.all()
-
+            query = SampleForm.objects.all()
         else:
             raise PermissionDenied("You do not have permission to access this resource.")
+        return query.order_by("-created_date")
         
     def get(self, request, format=None):
 
