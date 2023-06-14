55d45b3de4531c0786e5a86c0073e6ae66082778
mddas <mddas>
2023-06-14 17:54:01 +0545

    response status

--- report/status.py
+++ report/status.py
@@ -8,23 +8,43 @@ from management import roles
 from rest_framework.exceptions import PermissionDenied
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.filters import SearchFilter,OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
+
 
 class GetStatus(views.APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
         
     def get(self, request, format=None):
         import json
-        status = (
-            ('pending', 'pending'),
-            ('processing', 'processing'), 
-            ('not_assigned', 'not_assigned'),
-            ('not_verified', 'not_verified'),
-            ('completed', 'completed'),
-            ('recheck', 'recheck'),
-            ('rejected', 'rejected'),
-            )
-        data = json.loads(status)
-        return Response(data)
+        status = [
+            {
+                'name': 'Pending',
+                'value': 'pending', 
+            },
+            {
+                'name': 'Processing',
+                'value': 'processing', 
+            },
+            {
+                'name': 'Not Assigned',
+                'value': 'not_assigned',
+            },
+            {
+                'name': 'Not Verified',
+                'value': 'not_verified',
+            },
+            {
+                'name': 'Completed',
+                'value': 'completed',
+            },
+            {
+                'name': 'Recheck',
+                'value': 'recheck',
+            },
+            {
+                'name': 'Rejected',
+                'value': 'rejected',
+            }
+        ]
+
+        return Response(status)
