7c02120ab4da235c18033d5c3e24052835e1e3d0
mddas <mddas>
2023-06-15 12:25:57 +0545

    track filter

--- report/track.py
+++ report/track.py
@@ -10,8 +10,9 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import SearchFilter,OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
 
-class TrackSampleFormAPIView(views.APIView):
+class TrackSampleFormAPIView(generics.ListAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
@@ -38,16 +39,13 @@ class TrackSampleFormAPIView(views.APIView):
         else:
             raise PermissionDenied("You do not have permission to access this resource.")
         return query.order_by("-created_date")
-        
-    def get(self, request, format=None):
-
-        queryset = self.get_queryset()
-        # print(queryset)
-        # queryset = SampleForm.objects.filter(Q(verifier__is_sent=True) & Q(verifier__is_verified=False))
-
-        # queryset = SampleForm.objects.all()
+    
+    def get_serializer_class(self):
         if self.request.user.role == roles.ANALYST:
-            serializer = CompletedSampleFormHasAnalystSerializer(queryset, many=True)
+            serializer = CompletedSampleFormHasAnalystSerializer
         else:
-            serializer = CompletedSampleFormHasVerifierSerializer(queryset, many=True)
-        return Response(serializer.data)
+            serializer = CompletedSampleFormHasVerifierSerializer
+        return serializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
