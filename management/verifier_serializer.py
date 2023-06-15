a692cc8fcf7a4f42cb0dbdfba4fafdcc1e7019d9
mddas <mddas>
2023-06-15 12:55:59 +0545

    verified by

--- management/verifier_serializer.py
+++ management/verifier_serializer.py
@@ -10,6 +10,14 @@ class SampleFormWriteVerifierSerilizer(serializers.ModelSerializer):
         if data.get('sample_form') !=None :
             sample_form_id = data.get('sample_form').id
             sample_form_obj = SampleForm.objects.get(id=sample_form_id)
+
+            is_verified = data.get('is_verified')
+
+            if is_verified is not None and is_verified ==  True:
+                request = self.context.get('request')
+                sample_form_obj.verified_by = request.user
+                sample_form_obj.save()
+
             if sample_form_obj.is_analyst_test == False:
                 raise serializers.ValidationError("Analyst have not sent sample test to supervisor.")
 
