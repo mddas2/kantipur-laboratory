ac81a820c7437c4df0467ea0998b22d00416e77f
mddas <mddas>
2023-06-15 18:05:29 +0545

    verifiedd by fixed

--- management/verifier_serializer.py
+++ management/verifier_serializer.py
@@ -2,24 +2,31 @@ from .models import SampleFormVerifier,SampleForm
 from rest_framework import serializers
 
 class SampleFormWriteVerifierSerilizer(serializers.ModelSerializer):
-    class Meta:
-        model = SampleFormVerifier
-        fields = '__all__'
-
     def validate(self, data):
+    
         if data.get('sample_form') !=None :
             sample_form_id = data.get('sample_form').id
             sample_form_obj = SampleForm.objects.get(id=sample_form_id)
 
-            is_verified = data.get('is_verified')
-
-            if is_verified is not None and is_verified ==  True:
-                request = self.context.get('request')
-                sample_form_obj.verified_by = request.user
-                sample_form_obj.save()
-
             if sample_form_obj.is_analyst_test == False:
                 raise serializers.ValidationError("Analyst have not sent sample test to supervisor.")
+        
+        action = self.context['view'].action
+        if action == 'partial_update' or action == "update":
+            is_verified = data.get('is_verified')
 
+            if is_verified ==  True: 
+                instance = self.instance
+                sample_form_obj = instance.sample_form                
+                request = self.context.get('request')
+                sample_form_obj.verified_by = request.user                
+                sample_form_obj.save()
+
+        
         return data
     
+    class Meta:
+        model = SampleFormVerifier
+        fields = '__all__'
+
+ 
