6129cbcc49a764157a6fc2e45ea8f16974f21a04
mddas <mddas>
2023-06-20 14:00:00 +0545

    user request

--- websocket/frontend_setting.py
+++ websocket/frontend_setting.py
@@ -2,7 +2,7 @@
 main_url = "https://lims-home.netlify.app/"
 
 add_sample_form = "/dashboard/add-sample"
-user_request = "/dashboard/user-request"
+user_request = "/dashboard/user-requests"
 sample_request = "/dashboard/sample-requests"
 all_users = "/dashboard/all-users"
 client_category = "/dashboard/settings/client-category"
