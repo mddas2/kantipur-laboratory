338ffe548e89a5f343cf1075c7639d7764664d9f
mddas <mddas>
2023-06-21 22:44:37 +0545

    partial update

--- management/views.py
+++ management/views.py
@@ -176,6 +176,23 @@ class SampleFormViewSet(viewsets.ModelViewSet):
 
         # Return the custom response
         return Response(response_data)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        # Save the updated object to the database
+        self.perform_update(serializer)
+
+        # Create a custom response
+        response_data = {
+            "message": "Sample partially updated successfully",
+            "data": serializer.data
+        }
+
+        # Return the custom response
+        return Response(response_data, status=status.HTTP_200_OK)
     
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
