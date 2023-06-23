98f21eae4c252a05ccfded36795d17a96fb23e48
mddas <mddas>
2023-06-24 00:21:52 +0545

    raw data

--- management/raw_data.py
+++ management/raw_data.py
@@ -1,15 +1,23 @@
 from .models import RawDataSheet,RawDataSheetDetail,TestResult,SampleFormHasParameter
 def generateRawData(sample_form_has_parameter_id):
     obj = SampleFormHasParameter.objects.get(sample_form_has_parameter_id)
-    parameters = obj.parameter.all()
+
+    formula_calculate_parameters = obj.formula_calculate.all()
 
     sample_form_id = obj.sample_form.id
     
     raw_data_sheet_instance = RawDataSheet(sample_form_id=sample_form_id,sample_form_has_parameter_id = obj.id)
     raw_data_sheet_instance.save()
 
-    for param in parameters:
-        # param_id = 
-        pass
-    id = raw_data_sheet_instance.id
+    for param in formula_calculate_parameters:
+        data = {
+            'parameter':param.parameter.id,
+            'result':param.result,
+            'is_verified':param.is_verified,
+            'input_fields_value':param.input_fields_value,
+            'auto_calculate_result':param.auto_calculate_result,
+            'remark':param.remark,
+        }
+        RawDataSheetDetail.objects.update_or_create(raw_data=raw_data_sheet_instance.id,defaults=data)
+        
     
