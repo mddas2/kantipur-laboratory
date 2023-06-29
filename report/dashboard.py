825d8437676d10cfca2323d7fe07299360cd1d37
mddas <mddas>
2023-06-29 13:38:33 +0545

    rgba

--- report/dashboard.py
+++ report/dashboard.py
@@ -54,11 +54,12 @@ class reportStatus(views.APIView):
 
         elif self.request.user.role == roles.USER:
             total_users = 0#CustomUser.objects.all().count()
-            total_sample_forms = SampleForm.objects.filter(owner_user = self.request.user.email).count()
-            total_report_generated = total_sample_forms.filter(verifier__is_verified=True).count()
-            not_verified = total_sample_forms.filter(verifier__is_verified=False).count()
-            recheck = total_sample_forms.filter(raw_datasheet__status="rechecks").count()
-            pending = total_sample_forms.filter(status = "pending")
+            total_sample_forms_obj = SampleForm.objects.filter(owner_user = self.request.user.email).count()
+            total_sample_forms = total_sample_forms_obj.count()
+            total_report_generated = total_sample_forms_obj.filter(verifier__is_verified=True).count()
+            not_verified = total_sample_forms_obj.filter(verifier__is_verified=False).count()
+            recheck = total_sample_forms_obj.filter(raw_datasheet__status="rechecks").count()
+            pending = total_sample_forms_obj.filter(status = "pending")
             data = {
                 'total_request':total_sample_forms,
                 'not_verified':not_verified,
