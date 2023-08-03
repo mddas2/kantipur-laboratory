f5e521acd7ae6c374b1b1431ba8609d47763965e
mddas2 <manojdas.py@gmail.com>
2023-08-03 11:46:07 +0545

    upload import

--- management/import_excel.py
+++ management/import_excel.py
@@ -5,6 +5,7 @@ import os
 from rest_framework.response import Response
 import pandas as pd
 from django.http import JsonResponse
+from django.shortcuts import render
 
 from .models import Commodity,CommodityCategory,TestResult,SampleForm
 
@@ -13,106 +14,115 @@ def ImportExcel(request):
     # SampleForm.objects.all().delete()
     # return HttpResponse("deleted successfully!!!....")
     
-    file_path = os.path.join(settings.MEDIA_ROOT, "import_data/water2.xlsx")
-    print(file_path)
+    if request.POST:
+        file = request.FILES.get('file')
+    else:
+        return render(request, 'excel_import.html')
 
-    if os.path.exists(file_path):
-        df = pd.read_excel(file_path)
-        for index, row in df.iterrows():
-            
-            commodity_category = row['commodity_category']
-            commodity_category_nepali = row['commodity_cat_nepali']
+    file_path = file
+ 
+    df = pd.read_excel(file_path)
+    for index, row in df.iterrows():
+        
+        commodity_category = row['commodity_category']
+        commodity_category_nepali = row['commodity_cat_nepali']
 
-            commodity_name = row['commodity_name']
-            commodity_name_nepali = row['commodity_name_nepali']
+        commodity_name = row['commodity_name']
+        commodity_name_nepali = row['commodity_name_nepali']
 
-            test_type = row['test_type']
-            test_type_nepali = row['test_type_nepali']
+        test_type = row['test_type']
+        test_type_nepali = row['test_type_nepali']
 
-            parameters_name = row['parameters']
-            try:
-                parameters_nepali = row['parameter_nepali']
-            except:
-                parameters_nepali = parameters_name
+        parameters_name = row['parameters']
+        try:
+            parameters_nepali = row['parameter_nepali']
+        except:
+            parameters_nepali = parameters_name
 
-            ref_test_method = row['ref._test_methods']
+        ref_test_method = row['ref._test_methods']
 
-            commodity_test_duration = row['commodity_test_duration']
+        commodity_test_duration = row['commodity_test_duration']
 
-            unit = row['units']
-            unit_nepali = row['units_nepali']
+        unit = row['units']
+        unit_nepali = row['units_nepali']
 
-            mandatory_standard = row['mandatory_standard']
-       
-            mandatory_standard_nepali = row['mandatory_standard_nepali']
+        mandatory_standard = row['mandatory_standard']
+    
+        mandatory_standard_nepali = row['mandatory_standard_nepali']
 
-            formula = row['formula']
+        formula = row['formula']
 
-            try:
-                notation = row['abbreviation']
-            except:
-                notation = ""
+        try:
+            notation = row['abbreviation']
+        except:
+            notation = ""
 
-            remarks = row['remarks']
+        remarks = row['remarks']
 
-            commodity_price = row['commodity_price']
-            try:
-                commodity_price = int(commodity_price)
-            except:
-                commodity_price = 0
-            
-            parameter_price = row['parameter_price']
-            try:
-                parameter_price = int(parameter_price)
-            except:
-                parameter_price = 0
-     
-            # return HttpResponse(parameter_price)
+        commodity_price = row['commodity_price']
+        try:
+            commodity_price = int(commodity_price)
+        except:
+            commodity_price = 0
+        
+        parameter_price = row['parameter_price']
+        try:
+            parameter_price = int(parameter_price)
+        except:
+            parameter_price = 0
+    
+        # return HttpResponse(parameter_price)
 
-            commodity_category_data = {
-                'name' : commodity_category,
-                'name_nepali' : commodity_category_nepali,
-            }
+        commodity_category_data = {
+            'name' : commodity_category,
+            'name_nepali' : commodity_category_nepali,
+        }
 
 
-            try:
-                commodity_category_obj = CommodityCategory.objects.create(**commodity_category_data)
-            except:
-                print("can not create")
-            commodity_category_id = CommodityCategory.objects.get(name=commodity_category).id
-            commodity_data = {
-                'category_id' : commodity_category_id,
-                'name' : commodity_name,
-                'name_nepali':commodity_name_nepali,
-                'units' : unit,
-                'price' : commodity_price,
-                'test_duration' : commodity_test_duration,
-            }
-            commodity_obj,create = Commodity.objects.update_or_create(name = commodity_name, defaults= commodity_data)
-            if create:
-                print("commodity created")
-            commodity_id = Commodity.objects.get(name=commodity_name).id
-            test_result = { #parameter
-                'commodity_id' : commodity_id,
-                'name' : parameters_name,
-                'name_nepali' : parameters_nepali,
-                'ref_test_method' : ref_test_method,
-                'units' : unit,
-                'units_nepali' : unit_nepali,
-                'price' : parameter_price,
-                'mandatory_standard' : mandatory_standard,
-                'mandatory_standard_nepali' : mandatory_standard_nepali,
-                'remarks' : remarks,
-                'test_type' : test_type,
-                'test_type_nepali' : test_type_nepali,
-                'formula_notation' : notation,
-                'formula' : formula,
-            }
-            testresult_obj,create_test_result = TestResult.objects.update_or_create(commodity_id = commodity_id ,name = parameters_name, defaults = test_result)
-            if create_test_result:
-                print("parameter created successfully")
+        try:
+            commodity_category_obj = CommodityCategory.objects.create(**commodity_category_data)
+        except:
+            print("can not create")
+        commodity_category_id = CommodityCategory.objects.get(name=commodity_category).id
+        commodity_data = {
+            'category_id' : commodity_category_id,
+            'name' : commodity_name,
+            'name_nepali':commodity_name_nepali,
+            'units' : unit,
+            'price' : commodity_price,
+            'test_duration' : commodity_test_duration,
+        }
+        commodity_obj,create = Commodity.objects.update_or_create(name = commodity_name, defaults= commodity_data)
+        if create:
+            print("commodity created")
 
-        top_10 = df.head(10)
-        # print(top_10)
-        data = top_10.to_json(orient='records')
-        return JsonResponse(data, safe=False)
+        multiple_units,multiple_mandatory_standard,multiple_ref_test_method = multipleUnitsMandatoryRefTestMethod(unit,unit_nepali,ref_test_method,mandatory_standard,mandatory_standard_nepali)
+
+        commodity_id = Commodity.objects.get(name=commodity_name).id
+        test_result = { #parameter
+            'commodity_id' : commodity_id,
+            'name' : parameters_name,
+            'name_nepali' : parameters_nepali,
+            'ref_test_method' : ref_test_method,
+            'units' : unit,
+            'units_nepali' : unit_nepali,
+            'price' : parameter_price,
+            'mandatory_standard' : mandatory_standard,
+            'mandatory_standard_nepali' : mandatory_standard_nepali,
+            'remarks' : remarks,
+            'test_type' : test_type,
+            'test_type_nepali' : test_type_nepali,
+            'formula_notation' : notation,
+            'formula' : formula,
+        }
+        testresult_obj,create_test_result = TestResult.objects.update_or_create(commodity_id = commodity_id ,name = parameters_name, defaults = test_result)
+        if create_test_result:
+            print("parameter created successfully")
+
+    top_10 = df.head(10)
+    # print(top_10)
+    data = top_10.to_json(orient='records')
+    return JsonResponse(data, safe=False)
+
+def multipleUnitsMandatoryRefTestMethod(unit,unit_nepali,ref_test_method,mandatory_standard,mandatory_standard_nepali):
+    return [],[],[]
