cf3ef850b51b8421258223c157a392976a5106f0
mddas2 <manojdas.py@gmail.com>
2023-09-13 12:07:28 +0545

    nepali date in report

--- management/templatetags/json_custom_tag.py
+++ management/templatetags/json_custom_tag.py
@@ -1,5 +1,8 @@
 from django import template
 import json
+import datetime
+import nepali_datetime
+from django.template.defaulttags import register
 
 register = template.Library()
 
@@ -12,4 +15,14 @@ def json_to_dict(value):
     except (ValueError, TypeError):
         pass
     return {}
+
+@register.filter
+def get_NepaliDate(date):
+    try:
+        dt = datetime.date(date.year,date.month,date.day)
+        nepali_date = nepali_datetime.date.from_datetime_date(dt)
+        return nepali_date
+        # return nepali_date.strftime('%K-%n-%D (%k %N %G)')   
+    except:
+        return date
 
