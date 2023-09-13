6f9c553fa15b10630665b7c2efbbf5301efb9613
mddas2 <manojdas.py@gmail.com>
2023-09-13 12:52:55 +0545

    nepali datetime

--- report/templatetags/custom_tags.py
+++ report/templatetags/custom_tags.py
@@ -1,15 +1,14 @@
 import datetime
-# import nepali_datetime
 from django.template.defaulttags import register
 import json
 
 @register.filter
 def get_NepaliDate(date):
     try:
-        return date
-        # dt = datetime.date(date.year,date.month,date.day)
-        # nepali_date = nepali_datetime.date.from_datetime_date(dt)
-        # return nepali_date
+        import nepali_datetime
+        dt = datetime.date(date.year,date.month,date.day)
+        nepali_date = nepali_datetime.date.from_datetime_date(dt)
+        return nepali_date
         # return nepali_date.strftime('%K-%n-%D (%k %N %G)')   
     except:
         return date
