97b1bb6c83e2adeed4b8e6b54dda6e788ca80f56
mddas2 <manojdas.py@gmail.com>
2023-11-01 11:50:51 +0545

    email enabled

--- emailmanagement/sendmail_final_report.py
+++ emailmanagement/sendmail_final_report.py
@@ -18,8 +18,8 @@ def sendFinalreport(instance):
                     <td align="center" bgcolor="#FFFFFF" style="padding: 20px;">
                         <img src="http://lims.dftqc.gov.np/assets/nepal-government.png" alt="" width="132" style="display: block; margin: 0 auto;">
                         <p style="color: #0B53A7; font-weight: 600; font-size: 18px; margin-top: 20px;">Labrotary Information Management System (LIMS)</p>
-                        <p style="text-align: center; font-weight: 400;">Your test for <span style="font-weight: 600;">{instance.name} (Reference Number: {instance.refrence_number})</span> is completed successfully. Please click here to view your report.</p>
-                        <a href="https://pdfmachine.kantipurinfotech.com.np/public/api/show-pdf-report/{instance.id}/1" style="text-decoration: none; background: #0B53A7; color: #FFFFFF; padding: 10px 20px; border-radius: 3px; display: inline-block; margin-top: 15px;">View Report</a>
+                        <p style="text-align: center; font-weight: 400;">Your test for <span style="font-weight: 600;">{instance.name} (Reference Number: {instance.refrence_number})</span> is completed successfully. Please visit DFTQC to collect your report.</p>
+                       
                         <p style="text-align: center; margin-top: 20px;">Please visit <a href="http://lims.dftqc.gov.np" style="text-decoration: none; color: #0B53A7; font-weight: 600;">www.lims.dftqc.gov.np</a> for any enquiries.</p>
                         <p style="margin: 0; text-align: center;"><span style="font-weight: 600;">Tel:</span> 977-1-4262369, 4262430, 4240016, 4262739</p>
                         <p style="margin: 0; text-align: center; text-decoration: none;"><span style="font-weight: 600;">Fax:</span> 977-1-4262337 <span style="font-weight: 600; margin-left: 10px;">E-mail:</span> info@dftqc.gov.np</p>
@@ -32,5 +32,5 @@ def sendFinalreport(instance):
     recipient_list = [instance.owner_user]
     plain_message = ""
     subject = f"Test report for {instance.name} (Refrence Number:{instance.refrence_number})"
-    # send_mail(subject, plain_message, email_from, recipient_list,html_message=message)
-    
+    send_mail(subject, plain_message, email_from, recipient_list,html_message=message)
+    #  <a href="https://pdfmachine.kantipurinfotech.com.np/public/api/show-pdf-report/{instance.id}/1" style="text-decoration: none; background: #0B53A7; color: #FFFFFF; padding: 10px 20px; border-radius: 3px; display: inline-block; margin-top: 15px;">View Report</a>
