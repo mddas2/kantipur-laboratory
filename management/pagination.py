6383c59942c05eef5b631edf5a8b222876c7e65b
vrittech-manoj <manoj@vrittechnologies.com>
2024-02-07 11:54:28 +0545

    page number pagination

--- management/pagination.py
+++ management/pagination.py
@@ -1,8 +1,12 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import LimitOffsetPagination,PageNumberPagination
 
 class MyLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 20
-    limit_query_param = "limit"
-    offset_query_param = "offset"
-    max_limit = 500 #max-limit set
+    limit_query_param = "limit" #upto data
+    offset_query_param = "offset" #which page
+    max_limit = 500
 
+class PageNumberPagination(PageNumberPagination):
+    page_size = 20  # Set your desired page size
+    page_size_query_param = 'limit'
+    max_page_size = 500  # Set the maximum page size
