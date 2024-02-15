90951ac664f9d35c7bee83eef842368b9f354e85
vrittech-manoj <manoj@vrittechnologies.com>
2024-02-15 12:32:00 +0545

    merge from main

--- report/final_serializer.py
+++ report/final_serializer.py
@@ -73,11 +73,7 @@ class FinalSampleFormReportSerializer(serializers.ModelSerializer):
     class Meta:
         name = "FinalSampleFormReportAPIViewReport"
         model = SampleForm
-<<<<<<< HEAD
         fields = ['id','supervisor_sample_form','name','supervisor_user','commodity','status','created_date','completed_date','client_category_detail','namuna_code'] #user access
-=======
-        fields = ['id','supervisor_sample_form','name','supervisor_user','sample_has_parameter_analyst','commodity','status','created_date','completed_date','client_category_detail','namuna_code','is_print'] #user access
->>>>>>> main
     
     def to_representation(self,instance):
         representation = super().to_representation(instance)
@@ -118,11 +114,7 @@ class FinalSampleFormReportSerializer_User(serializers.ModelSerializer):
     class Meta:
         name = "FinalSampleFormReportAPIView_User_Report"
         model = SampleForm
-<<<<<<< HEAD
         fields = ['id','supervisor_sample_form','name','supervisor_user','commodity','status','created_date','completed_date','client_category_detail','namuna_code','mfd','dfb','sample_quantity','sample_units','voucher_number','voucher_date','analysis_fee','parameters','days_dfb','dfb_duration'] #user access
-=======
-        fields = ['id','supervisor_sample_form','name','supervisor_user','sample_has_parameter_analyst','commodity','status','created_date','completed_date','client_category_detail','namuna_code','mfd','dfb','sample_quantity','sample_units','voucher_number','voucher_date','analysis_fee','parameters','days_dfb','dfb_duration','is_print'] #user access
->>>>>>> main
     
     def to_representation(self,instance):
         representation = super().to_representation(instance)
