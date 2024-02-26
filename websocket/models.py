fdbb5919253bcf99d8d5f529a51eb6a9021973c9
vrittech-manoj <manoj@vrittechnologies.com>
2024-02-26 12:11:09 +0545

    notification

--- websocket/models.py
+++ websocket/models.py
@@ -2,20 +2,26 @@ from django.db import models
 from account.models import CustomUser
 from django.utils import timezone
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 # Create your models here.
 
 class Notification(models.Model):
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE,null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type','object_id')
+
+    notification_type = models.CharField(max_length=200,null=True,default="push")
+
     notification_message = models.CharField(max_length=200,null=True)
-    particular_message = models.CharField(max_length=200,null=True)
-    from_notification = models.CharField(max_length=200,null=True)
+    particular_message = models.TextField(null=True)
+    from_notification = models.ForeignKey(CustomUser,related_name="notifications",on_delete=models.CASCADE,null=True)
     path = models.CharField(max_length=1000,null=True)
-    model_name = models.CharField(max_length=200,null=True)
-    notification_type = models.CharField(max_length=200,null=True)
-    instance_id = models.CharField(max_length=200,null=True)
+
     to_notification = models.ManyToManyField(CustomUser,related_name="notification")
     is_read = models.BooleanField(default=False)
 
     group_notification = models.CharField(max_length = 1000,blank=True, null=True)
-    
     created_date = models.DateTimeField(auto_now_add=True)  
     updated_date = models.DateTimeField(auto_now=True)
