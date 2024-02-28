5b90ec631cdd6f6f338d0276a369757d4b634056
vrittech-manoj <manoj@vrittechnologies.com>
2024-02-28 12:34:32 +0545

    final report,assigned sample

--- report/assigned_sample_for_smu_superadmin.py
+++ report/assigned_sample_for_smu_superadmin.py
@@ -37,10 +37,8 @@ class FinalSampleFormHasVerifiedAPIView(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user 
        
-        if user.role == roles.SMU:
-            query = SampleForm.objects.filter(Q(status="processing") | Q(status="not_verified"))
-        elif user.role == roles.SUPERADMIN:
-            query = SampleForm.objects.filter(Q(status="processing") | Q(status="not_verified"))
+        if user.role in [roles.SMU,roles.SUPERADMIN]:
+            query = SampleForm.objects.filter(Q(status="processing") | Q(status="not_verified") | Q(status="not_approved"))
         else:
             raise PermissionDenied("You do not have permission to access this resource.")
         return query.order_by("-created_date")
